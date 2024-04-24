b1bc0bc32cf8cf282fc4422d34779ba412d9914d
Your Name <you@example.com>
2024-04-24 22:35:08 +0100

    prep wk2 done

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 import express from "express"; 
+import movies from "./moviesdata.json" assert {type: "json"};
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req,res) => {
     res.send("Hello")
+})
+
+app.get("/movies", (req,res) => {
+    res.send({movies});
+})
+
+app.get("/movies/:id", (req,res) => {
+    const movieId = Number(req.params.id);
+    const movie = movies.find((movie) => movie.id === movieId);
+    res.send(movie);
+})
+
+app.post("/movies", (req,res) => {
+    const newMovie = req.body;
+    movies.push(newMovie);
+    res.send(newMovie)
 })
 
 app.listen(3000, () => {
